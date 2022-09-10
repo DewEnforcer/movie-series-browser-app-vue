d731162baa16892e7a3ae3a437eab81e21705994
DewEnforcer <piranha863@gmail.com>
2022-09-10 14:57:27 +0200

    Add loader, nav item components

---
+++ v@3/src/components/header/NavItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li class="nav-item">
+    <router-link class="parsedClass" to="path">{{label}}</router-link>
+  </li>
+</template>
+
+<script>
+export default {
+    props: ["path", "label", "className"],
+    data() {
+        return {
+            parsedClass: ""
+        }
+    },
+    created() {
+        this.parseElementClass();
+    },
+    methods: {
+        parseElementClass() {
+            let cls = "nav-link";
+            if (this.className) cls = `${cls}  ${className}`;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
