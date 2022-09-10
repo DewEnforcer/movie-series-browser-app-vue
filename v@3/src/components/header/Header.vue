36c717a26a77e624ec7c47ea5733839dd3929f30
DewEnforcer <piranha863@gmail.com>
2022-09-10 15:07:02 +0200

    Add header and navigation components

---
+++ v@3/src/components/header/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <navigation :navItems="navItems"></navigation>
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation.vue';
+import { useRoute } from 'vue-router';
+
+export default {
+  components: { Navigation },
+    props: {
+        navItems: {
+            type: Array,
+            default: []
+        }
+    },
+    data() {
+        return {
+            visible: true
+        }
+    },
+    created() {
+        this.setRender();
+    },
+    methods: {
+        setRender() {
+            return (this.visible = !useRoute().name.includes("play"));
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ v@3/src/components/header/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+        <nav-item v-for="item in navItems" :key="item.id" :className="item.className" :path="item.path" :label="item.label"></nav-item>
+    </ul>
+</template>
+
+<script>
+import NavItem from "./NavItem.vue";
+
+export default {
+    components: {
+        NavItem
+    },
+    props: ["navItems"]
+}
+</script>
+
+<style>
+
+</style>
