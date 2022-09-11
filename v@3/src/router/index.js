9b19063cca54f88ad98ff6c1343d469b4fea091f
DewEnforcer <piranha863@gmail.com>
2022-09-11 11:45:43 +0200

    Add new routes

--- v@3/src/router/index.js
+++ v@3/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import MovieView from "../views/MovieView.vue";
+import SearchView from "../views/SearchView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/search",
+      name: "search",
+      component: SearchView,
+    },
+    {
+      path: "/movie",
+      name: "movie",
+      component: MovieView,
     },
   ],
 });
